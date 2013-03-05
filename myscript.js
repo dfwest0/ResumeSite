7804314389e0cea9041af5b76c9297eb012d055b
Devin West <west.devin@gmail.com>
2013-03-05 11:17:12 -0600

    Commit of the basic site files
    The index.html, myscript.js, and mystyle.css were added

---
+++ myscript.js
@@ -0,0 +1,14 @@
+/*$(document).ready(function() {
+  $('#objective').click(function(){
+    $('#objective p').slideToggle();
+  })
+});*/
+$(document).ready(function(){
+    $('#content').accordion({collapsible: true ,header: "h2", heightStyle: "content" });
+});
+/*var docHeight = $(window).height();
+console.log(docHeight);
+var htfun = function () {
+  alert(docHeight)
+};*/
+  
